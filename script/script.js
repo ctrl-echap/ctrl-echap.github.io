40c025e837a080b71902ac70be5c9b51808b32a0
hiboubou <meuana30@ecolecatholique.ca>
2025-05-10 12:42:09 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
-
+function salle1Question1(reponse1) {
+    console.log("Quelle  est valeur de x dans l'équation: ")
+    console.log("7x - 2 + 55x - 7 = 301")
+    reponse == 5
+    validation == false
+    while (validation == false) {
+        reponseInsere = parseFloat(prompt("Insérez le mot de passe:")
+        if (reponseInsere != reponse) {
+            console.log("Ah non! Le mot de passe inséré ne marche pas!")
+            validation == false
+        } else {
+            validation == true
+        }
+    }
+}
