0140c4a3d4d8cce9e525463384fbcc9799cdb36e
raphael245 <lourap24@ecolecatholique.ca>
2025-05-18 21:12:57 -0400

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
-//Fonction question 1 : Ours en peluche
+// Fonction pour recommencer le jeu (pendant le jeu)
+function recommencer() {
+    var recommence = confirm("Êtes-vous sûr de vouloir recommencer le jeu ?")
+    if (recommence === true) {
+        sessionStorage.clear();
+        window.location.href = "index.html"; // Source : https://www.w3schools.com/howto/howto_js_redirect_webpage.asp 
+    }
+}
+
+// Fonction pour recommencer le jeu (après la fin du jeu)
+function recommencer2() {
+    sessionStorage.clear();
+    window.location.href = "index.html"; // Source : https://www.w3schools.com/howto/howto_js_redirect_webpage.asp 
+}
+
+//Fonction pour vérifier la réponse à la question 1 : Ours en peluche
 function salle1Question1() {
     // Chercher les données dans le formulaire
     var affiche = document.getElementById("divAfficheOurs");
     var entree = parseInt(document.getElementById("questionOurs").value);
-    var tblQ1 = document.getElementById("tblQ1")
+    var tblQ1 = document.getElementById("tblQ1");
 
     // Créer une variable pour le message à afficher dépendamment de la réponse entrée
     var message = "";
@@ -16,14 +31,14 @@ function salle1Question1() {
         tblQ1.style.display = "none";
     } else {
         // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
-        message = "Ah non! désolé mauvaise réponse! Veuillez réesayer";
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
     }
         
     // On affiche le message sur la page web 
     affiche.innerHTML = message;
 }
 
-// Fonction question 2 : Plante
+// Fonction pour vérifier la réponse à la question 2 : Plante
 function salle1Question2(){
     // Chercher les données dans le formulaire
     var affiche = document.getElementById("divAffichePlante");
@@ -41,14 +56,14 @@ function salle1Question2(){
         tblQ2.style.display = "none";
     } else {
         // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
-        message = "Ah non! désolé mauvaise réponse! Veuillez réesayer";
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
     }
     
     // On affiche le message sur la page web 
     affiche.innerHTML = message;
 }
 
-//Fonction question 3 : Peinture
+//Fonction pour vérifier la réponse à la question 3 : Peinture
 function salle1Question3(){
     // Chercher les données dans le formulaire
     var affiche = document.getElementById("divAffichePeinture");
@@ -66,30 +81,14 @@ function salle1Question3(){
         tblQ3.style.display = "none";
     } else {
         // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
-        message = "Ah non! désolé mauvaise réponse! Veuillez réesayer";
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
     }
     
     // On affiche le message sur la page web 
     affiche.innerHTML = message;
 }
 
-//fonction de la porte pour la verification
-/*function verification(){
-    var reponse1=parseInt(document.getElementById("reponse1").value);
-    var reponse2=parseInt(document.getElementById("reponse2").value);
-    var reponse3=parseInt(document.getElementById("reponse3").value);
-    var reponse4=parseInt(document.getElementById("reponse4").value);
-    var reponse5=parseInt(document.getElementById("reponse5").value);
-    var affiche=document.getElementById("divaffiche7");
-    if ((reponse1==5)&&(reponse2==9)&&(reponse3==4)&&(reponse4==)&&(reponse5==)){
-    window.location.href = "chambre2vue1.html";
-    }else{
-      var message="non desole,vos entrées sont incorrectes! retournez revoir le bon ordre et réessayer";
-       affiche.innerHTML=message;
-    }
-}*/
-
-//Fonction question 4 : Tiroir
+//Fonction pour vérifier la réponse à la question 4 : Tiroir
 function salle2Question1(){
     // Chercher les données dans le formulaire
     var affiche = document.getElementById("divAfficheTiroir");
@@ -100,21 +99,21 @@ function salle2Question1(){
     var message = "";
     
     // Vérifier si c'est la bonne réponse et changer le message en conséquence
-    if (entree == 0) {
+    if (entree === 0) {
         // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
-        message = "🗄= 0";
+        message = "🗃️ = 0";
         sessionStorage.setItem("questionTiroirFait", "true");
         tblQ4.style.display = "none";
     } else {
         // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
-        message = "Ah non! désolé mauvaise réponse! Veuillez réesayer";
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
     }
     
     // On affiche le message sur la page web 
     affiche.innerHTML = message;
 }
 
-//Fonction question 5 : Cadre
+//Fonction pour vérifier la réponse à la question 5 : Cadre
 function salle2Question2(){
     // Chercher les données dans le formulaire
     var affiche = document.getElementById("divAfficheCadre");
@@ -127,18 +126,44 @@ function salle2Question2(){
     // Vérifier si c'est la bonne réponse et changer le message en conséquence
     if (entree == 28) {
         // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
-        message = "🖼 = 28";
+        message = "📈 = 28";
         sessionStorage.setItem("questionCadreFait", "true");
         tblQ5.style.display = "none";
     } else {
         // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
-        message = "Ah non! désolé mauvaise réponse! Veuillez réesayer";
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
     }
     
     // On affiche le message sur la page web 
     affiche.innerHTML = message;
 }
 
+// Fonction pour vérifier la réponse à la question 6 : Papier
+function salle2Question3(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAffichePapier");
+    var entree = parseInt(document.getElementById("questionPapier").value);
+    var tblQ6 = document.getElementById("tblQ6");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 4) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "📄 = 4";
+        sessionStorage.setItem("questionPapierFait", "true");
+        tblQ6.style.display = "none";
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// On crée la fonction pour la sortie de la chambre 1
 function validerSortie1(){
     // Chercher les données dans le formulaire
     var affiche = document.getElementById("divAffichePorte1");
@@ -147,14 +172,239 @@ function validerSortie1(){
     // Créer une variable pour le message à afficher dépendamment de la réponse entrée
     var message = "";
     
-    // Vérifier si c'est la bonne réponse et changer le message en conséquence
-    if (entree == 58) {
+    // Créer des variables qui gardent en mémoire quelles questions ont été répondues correctement
+    var oursFait = sessionStorage.getItem("questionOursFait")
+    var planteFait = sessionStorage.getItem("questionPlanteFait")
+    var peintureFait = sessionStorage.getItem("questionPeintureFait")
+    var tiroirFait = sessionStorage.getItem("questionTiroirFait")
+    var cadreFait = sessionStorage.getItem("questionCadreFait")
+    var papierFait = sessionStorage.getItem("questionPapierFait")
+    
+    // Vérifier que toutes les questions ont été répondues avant d'essayer de quitter la salle
+    if (!((oursFait == "true") && (planteFait == "true") && (peintureFait == "true") && (tiroirFait == "true") && (cadreFait == "true") && (papierFait == "true"))) {
+        message = "Vous devez répondre à toutes les questions dans la salle avant de tenter de quitter. Chop Chop !!!"
+    } else if (entree == 62) { // Si l'entrée est bonne
         // Aller à la prochaine salle
         window.location.href = "chambre2vue1.html" // Source : https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+        sessionStorage.setItem("porte1Fait", "true");
     } else {
         // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
-        message = "Ah non! désolé mauvaise réponse! Veuillez réesayer";
-        // On affiche le message sur la page web 
-        affiche.innerHTML = message;
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
     }
+    
+    // On affiche le message à l'écran
+    affiche.innerHTML = message;
+}
+
+// Fonction pour vérifier la réponse à la question 7 : Four
+function salle3Question1(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAfficheFour");
+    var entree = parseInt(document.getElementById("questionFour").value);
+    var tblQ7 = document.getElementById("tblQ7");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 73) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "🥧 = 73";
+        sessionStorage.setItem("questionFourFait", "true");
+        tblQ7.style.display = "none";
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// Fonction pour vérifier la réponse à la question 8 : Statue d'ours
+function salle3Question2(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAfficheOurs2");
+    var entree = parseInt(document.getElementById("questionOurs2").value);
+    var tblQ8 = document.getElementById("tblQ8");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 18) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "🍬 = 18";
+        sessionStorage.setItem("questionOurs2Fait", "true");
+        tblQ8.style.display = "none";
+
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// Fonction pour vérifier la réponse à la question 9 : Tiroir 2
+function salle3Question3(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAfficheTiroir2");
+    var entree = parseInt(document.getElementById("questionTiroir2").value);
+    var tblQ9 = document.getElementById("tblQ9");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 2) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "🥄 = 2";
+        sessionStorage.setItem("questionTiroir2Fait", "true");
+        tblQ9.style.display = "none";
+
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// Fonction pour vérifier la réponse à la question 10 : Bol 1
+function salle4Question1(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAfficheBolUn");
+    var entree = parseInt(document.getElementById("questionBolUn").value);
+    var tblQ10 = document.getElementById("tblQ10");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 4) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "🍚 = 4";
+        sessionStorage.setItem("questionBol1Fait", "true");
+        tblQ10.style.display = "none";
+
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// Fonction pour vérifier la réponse à la question 11 : Bol 2
+function salle4Question2(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAfficheBolDeux");
+    var entree = parseInt(document.getElementById("questionBolDeux").value);
+    var tblQ11 = document.getElementById("tblQ11");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 216) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "🍓 = 216";
+        sessionStorage.setItem("questionBol2Fait", "true");
+        tblQ11.style.display = "none";
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// Fonction pour vérifier la réponse à la question 12 : Bol 3
+function salle4Question3(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAfficheBolTrois");
+    var entree = parseInt(document.getElementById("questionBolTrois").value);
+    var tblQ12 = document.getElementById("tblQ12");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 2025) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "🥖 = 2025";
+        sessionStorage.setItem("questionBol3Fait", "true");
+        tblQ12.style.display = "none";
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// Fonction pour vérifier la réponse à la question 13 : L'affiche
+function salle4Question4(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAfficheAffiche");
+    var entree = parseInt(document.getElementById("questionAffiche").value);
+    var tblQ13 = document.getElementById("tblQ13");
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Vérifier si c'est la bonne réponse et changer le message en conséquence
+    if (entree == 180) {
+        // On affiche un message de félicitations pour avoir eu la bonne réponse ainsi qu'un indice. On mets en mémoire que la question est complétée et on la cache
+        message = "🍪 = 180";
+        sessionStorage.setItem("questionAfficheFait", "true");
+        tblQ13.style.display = "none";
+
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message sur la page web 
+    affiche.innerHTML = message;
+}
+
+// On crée la fonction pour la sortie de la chambre 2
+function validerSortie2(){
+    // Chercher les données dans le formulaire
+    var affiche = document.getElementById("divAffichePorte2");
+    var entree = parseInt(document.getElementById("echap2").value);
+    
+    // Créer une variable pour le message à afficher dépendamment de la réponse entrée
+    var message = "";
+    
+    // Créer des variables qui gardent en mémoire quelles questions ont été répondues correctement
+    var fourFait = sessionStorage.getItem("questionFourFait")
+    var ours2Fait = sessionStorage.getItem("questionOurs2Fait")
+    var tiroir2Fait = sessionStorage.getItem("questionTiroir2Fait")
+    var bol1Fait = sessionStorage.getItem("questionBol1Fait")
+    var bol2Fait = sessionStorage.getItem("questionBol2Fait")
+    var bol3Fait = sessionStorage.getItem("questionBol3Fait")
+    var afficheFait = sessionStorage.getItem("questionAfficheFait")
+    
+    // Vérifier que toutes les questions ont été répondues avant d'essayer de quitter la salle
+    if (!((fourFait == "true") && (ours2Fait == "true") && (tiroir2Fait == "true") && (bol1Fait == "true") && (bol2Fait == "true") && (bol3Fait == "true") && (afficheFait == "true"))) {
+        message = "Vous devez répondre à toutes les questions dans la salle avant de tenter de quitter. Chop Chop !!!"
+    } else if (entree == 26) { // Si l'entrée est bonne
+        // Quitter la maison
+        window.location.href = "pagefin1.html" // Source : https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+        sessionStorage.setItem("porte2Fait", "true");
+    } else {
+        // On affiche un message de qui dit de réessayer si l'utilisateur a entré la mauvaise réponse
+        message = "Ah non! désolé mauvaise réponse! Veuillez réessayer";
+    }
+    
+    // On affiche le message à l'écran
+    affiche.innerHTML = message;
 }
