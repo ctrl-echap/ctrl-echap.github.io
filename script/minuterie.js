f0fdaafef45eee11e5b906ec7f9a859b42b9f5f1
raphael245 <lourap24@ecolecatholique.ca>
2025-05-18 21:14:59 -0400

    Create minuterie.js

---
+++ script/minuterie.js
@@ -0,0 +1,35 @@
+// On définit le temps maximale comme étant de 30 minutes
+let totalTime = 30 * 60; 
+
+// On définit la fonction qui commence minuterie
+function startTimer() {
+    let time = sessionStorage.getItem("remainingTime");
+    if (time === null) {
+        time = totalTime;
+        sessionStorage.setItem("remainingTime", time);
+    } else {
+        time = parseInt(time);
+    }
+
+    const timerDisplay = document.getElementById("game-timer");
+
+    // On crée une fonction pour faire la mise à jour de la minuterie à la minute et la seconde précise
+    function updateTimer() {
+        if (!timerDisplay) return;
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60; 
+        timerDisplay.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        time--;
+        sessionStorage.setItem("remainingTime", time);
+
+        // On met la condition d'arrêt du jeu si la minuterie frappe 0 secondes restants
+        if (time < 0) {
+            clearInterval(timerInterval);
+            window.location.href = "pagefin2.html"; 
+        }
+    }
+
+    // On définit l'intervalle de temps pour rappeler l'utilisateur du temps qu'il lui reste
+    updateTimer();
+    const timerInterval = setInterval(updateTimer, 1000);
+}
